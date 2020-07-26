d9b014c1734a6caecec8fbc78e89e47ae0b2dbb5
alonea <Rajesh.Alonea@proounce.com>
2020-07-26 21:44:32 +0530

    Updates

---
+++ precache-manifest.4d8008f671948d5d857cb8f125c74e6c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bc9803ed97f424ee13d498dbececf2a",
+    "url": "/vidhyarthi-in.github.io/index.html"
+  },
+  {
+    "revision": "69b2e70a99c27b2fa888",
+    "url": "/vidhyarthi-in.github.io/static/css/2.03c3e79b.chunk.css"
+  },
+  {
+    "revision": "58895bfe14f96927cbeb",
+    "url": "/vidhyarthi-in.github.io/static/css/main.372d31b3.chunk.css"
+  },
+  {
+    "revision": "69b2e70a99c27b2fa888",
+    "url": "/vidhyarthi-in.github.io/static/js/2.aa1555ce.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/vidhyarthi-in.github.io/static/js/2.aa1555ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58895bfe14f96927cbeb",
+    "url": "/vidhyarthi-in.github.io/static/js/main.084633aa.chunk.js"
+  },
+  {
+    "revision": "9b4f22915d8bd4571e70",
+    "url": "/vidhyarthi-in.github.io/static/js/runtime-main.0dab5273.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/vidhyarthi-in.github.io/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/vidhyarthi-in.github.io/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/vidhyarthi-in.github.io/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/vidhyarthi-in.github.io/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/vidhyarthi-in.github.io/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
